45544f5c2f3b8372108a69d0b0b33a0cce5932c3
krishkumar4400 <krishkumarbgp2022@gmail.com>
2025-12-06 16:39:05 +0530

    Deployment configuration

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -19,22 +20,45 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
+        best_model = None
+        best_model_name = None
+        best_score = -999
 
         for model_name, model in models.items():
-            model.fit(X_train, y_train)
 
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+            # Hyperparameter tuning
+            param_grid = params[model_name]
+            gs = GridSearchCV(model, param_grid, cv=3)
+            gs.fit(X_train, y_train)
 
-            train_model_score = r2_score(y_train, y_train_pred)
-            test_model_score = r2_score(y_test, y_test_pred)
+            # best estimator
+            best_estimator = gs.best_estimator_
+            best_estimator.fit(X_train, y_train)
 
-            report[model_name] = test_model_score
+            # prediction
+            y_pred = best_estimator.predict(X_test)
+            score = r2_score(y_test, y_pred)
 
-        return report
+            report[model_name] = score
+
+            # choose best model
+            if score > best_score:
+                best_score = score
+                best_model = best_estimator
+                best_model_name = model_name
+
+        return best_model_name, best_model, report
 
     except Exception as e:
         raise CustomException(e, sys)
+   
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+        
